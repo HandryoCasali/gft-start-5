722db47e79ebc904be2690c29125f8d8c0feef03
Handryo Casali <handryo@hotmail.com>
2022-07-19 18:32:12 -0300

    Corrigindo bug mensagem inicial

--- Exercicio01/Program.cs
+++ Exercicio01/Program.cs
@@ -9,10 +9,10 @@ namespace Exercicio01
             bool entrada = true;
             do
             {
-                Console.WriteLine($"Digite um numero entre 0 e 400");
+                Console.WriteLine($"Digite um numero entre 0 e 4000");
                 
                 numeroDigitado = int.Parse(Console.ReadLine());
-                if(numeroDigitado < 0 || numeroDigitado >= 4000)
+                if(numeroDigitado <= 0 || numeroDigitado > 4000)
                 {
                     System.Console.WriteLine("Numero precisa ser maior que 0 e menor que 400");
                     entrada = false;
