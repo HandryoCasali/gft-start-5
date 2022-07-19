cbaa768dd2d0060d4197af1a2eed79a41dffdb40
Handryo Casali <handryo@hotmail.com>
2022-07-19 18:10:48 -0300

    Adicionando exercicio 1

--- Exercicio01/Program.cs
+++ Exercicio01/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace Exercicio01
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int numeroDigitado;
+            bool entrada = true;
+            do
+            {
+                Console.WriteLine($"Digite um numero entre 0 e 400");
+                
+                numeroDigitado = int.Parse(Console.ReadLine());
+                if(numeroDigitado < 0 || numeroDigitado >= 400)
+                {
+                    System.Console.WriteLine("Numero precisa ser maior que 0 e menor que 400");
+                    entrada = false;
+                }
+            } while (!entrada);
+            Console.Write($"Numero {numeroDigitado} em Numero Romano: ");
+            
+            ImprimirNumeroEmRomano(numeroDigitado);
+        }
+
+        public static void ImprimirNumeroEmRomano(int numero)
+        {
+            int[] num = new int[]{1, 4, 5, 9, 10, 40, 50, 90,
+                            100, 400, 500, 900, 1000};
+            string[] letras = new string[]{"I", "IV", "V", "IX", "X", "XL",
+            "L", "XC", "C", "CD", "D", "CM", "M"};
+
+            for(int i = 12; i > 0; i-- )
+            {
+                int div = (int)(numero / num[i]);
+                numero %= num[i];
+
+                for(int j = div; div > 0; div--)
+                {
+                    Console.Write(letras[i]);
+                }
+            }
+        }
+    }
+}
--- Exercicio03/Program.cs
+++ Exercicio03/Program.cs
@@ -1,1 +1,14 @@
 ﻿
+using System;
+
+namespace Exercicio01
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Não consegui fazer, deixei por ultimo e não deu tempo!");
+        }
+
+    }
+}
